5e0540dd0ee751ba77804a70f23e99c83d20d689
wangyabo321 <wangyabo321@163.com>
2022-08-31 22:58:53 +0800

    新增123

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,52 +1,52 @@
 <template>
-    <div>
-        <HomeHeader></HomeHeader>
-        <HomeSwiper :swiperLists="swiperLists"></HomeSwiper>
-        <HomeIcon :iconLists="iconLists"></HomeIcon>
-        <HomeRecommend :recommendLists="recommendLists"></HomeRecommend>
-        <HomeWeekend :weekendLists="weekendLists"></HomeWeekend>
-    </div>
+  <div>
+    <div>12323132132132123</div>
+    <HomeHeader></HomeHeader>
+    <HomeSwiper :swiperLists="swiperLists"></HomeSwiper>
+    <HomeIcon :iconLists="iconLists"></HomeIcon>
+    <HomeRecommend :recommendLists="recommendLists"></HomeRecommend>
+    <HomeWeekend :weekendLists="weekendLists"></HomeWeekend>
+  </div>
 </template>
 
 <script>
-    import HomeHeader from './components/homeheader'
-    import HomeSwiper from './components/homeswiper'
-    import HomeIcon from './components/homeicon'
-    import HomeRecommend from './components/homerecommend'
-    import HomeWeekend from './components/homeweekend'
-    export default {
-        name: "home",
-        data(){
-            return{
-                swiperLists:[],
-                iconLists:[],
-                recommendLists:[],
-                weekendLists:[]
-            }
-        },
-        components:{
-            HomeHeader,
-            HomeSwiper,
-            HomeIcon,
-            HomeRecommend,
-            HomeWeekend
-        },
-        created() {
-            //请求数据
-            this.$axios.get('../../../static/mock/index.json')
-                .then(res => {
-                    this.swiperLists = res.data.data.swiperList;
-                    this.iconLists = res.data.data.iconList;
-                    this.recommendLists = res.data.data.recommendList;
-                    this.weekendLists = res.data.data.weekendList;
-                })
-                .catch(error => {
-                   console.log(error);
-                })
-        }
-    }
+import HomeHeader from "./components/homeheader";
+import HomeSwiper from "./components/homeswiper";
+import HomeIcon from "./components/homeicon";
+import HomeRecommend from "./components/homerecommend";
+import HomeWeekend from "./components/homeweekend";
+export default {
+  name: "home",
+  data() {
+    return {
+      swiperLists: [],
+      iconLists: [],
+      recommendLists: [],
+      weekendLists: []
+    };
+  },
+  components: {
+    HomeHeader,
+    HomeSwiper,
+    HomeIcon,
+    HomeRecommend,
+    HomeWeekend
+  },
+  created() {
+    //请求数据
+    this.$axios
+      .get("../../../static/mock/index.json")
+      .then(res => {
+        this.swiperLists = res.data.data.swiperList;
+        this.iconLists = res.data.data.iconList;
+        this.recommendLists = res.data.data.recommendList;
+        this.weekendLists = res.data.data.weekendList;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
